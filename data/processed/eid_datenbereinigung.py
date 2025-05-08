5845559add0b0f6d2bc3ea853ace530a7132803e
Christina Shneydin <chrisi.shneydin@gmail.com>
2025-05-08 22:36:47 +0200

    create revenue_html, create revenue comparison notebook

--- data/processed/eid_datenbereinigung.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-file_path = r"C:\Users\chris\Corporate_Environmental_Impact\data\raw\Scope-3-Environmental-Impact-Data-2022.xlsx"
-
-df = pd.read_excel(file_path, sheet_name="0%")
-
-print(df.head())
-df.shape
